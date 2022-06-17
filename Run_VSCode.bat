c4d54d82428f72ecd931b2d66d7c43de3359cdb8
tak_sas <takuminakayama4869@gmail.com>
2022-06-18 00:38:47 +0900

    First Commit

---
+++ Run_VSCode.bat
@@ -0,0 +1,2 @@
+code . -n
+exit
---
+++ practice/A.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c;
+    char s[100];
+    scanf("%d" ,&a);
+    scanf("%d %d" ,&b, &c);
+    scanf("%s" , s);
+    printf("%d %s", a + b + c, s);
+    return 0;
+}
