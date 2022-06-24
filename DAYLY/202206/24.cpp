b56dc0f2b63396b358ee564678ad819c36e83a8c
tak_sas <takuminakayama4869@gmail.com>
2022-06-24 15:26:53 +0900

    2022/06/24

---
+++ DAYLY/202206/24.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N, Q; cin >> N >> Q;
+    vector<int> A(N);
+
+    for(auto& a : A) {
+        cin >> a;
+    }
+
+    sort(A.begin(), A.end());
+
+    
+    for (int i = 0; i < Q; i++) {
+        int point, ok = N, ng = -1, temp; 
+        cin >> temp;
+        while ((ok - ng) > 1) {
+            point = (ok + ng) / 2;
+            if(A[point] >= temp) {
+                ok = point;
+            } else {
+                ng = point;
+            }
+        }
+        cout << N - ok << endl;
+    }
+    return 0;
+}
