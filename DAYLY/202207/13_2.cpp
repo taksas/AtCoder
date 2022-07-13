f487dd3045820e30558498e54b3cb95abcd547c9
taksas <takuminakayama4869@gmail.com>
2022-07-13 13:26:59 +0900

    2022/07/13_2

---
+++ DAYLY/202207/13_2.cpp
@@ -0,0 +1,66 @@
+// ARC115 - C
+// 20%
+// 1st
+// LEAVE
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <tuple>
+#include <cassert>
+#include <bitset>
+
+
+#define rep(i,n)                for (int i = 0; i < n; i++)
+#define repe(i,n)               for (int i = 0; i <= n; i++)
+#define rrep(i,n)               for (int i = n; i > 0; i--)
+#define rrepe(i,n)              for (int i = n; i >= 0; i--)
+#define vrep(X)                 for (auto&& x : X)
+
+#define vvii(X,n1,n2)            vector<vector<int>> X(n1,vector<int>(n2))
+#define vvllll(X,n1,n2)         vector<vector<long long int>> X(n1,vector<long long int>(n2))
+
+#define vpii(X)                 vector<pair<int, int>> X
+#define vpss(X)                 vector<pair<string, string>> X
+#define vpcc(X)                 vector<pair<char, char>> X
+#define vpis(X)                 vector<pair<int, string>> X
+#define vpic(X)                 vector<pair<int, char>> X
+#define vpsi(X)                 vector<pair<string, int>> X
+#define vpsc(X)                 vector<pair<string, char>> X
+#define vpci(X)                 vector<pair<char, int>> X
+#define vpcs(X)                 vector<pair<char, string>> X
+#define vint(X)                 vector<int> X
+#define vchar(X)                vector<string> X
+#define vstring(X)              vector<char> X
+
+#define bgen(X)                 begin(X), end(X)
+#define coutd(X)                cout << X << endl
+
+
+using namespace std;
+
+int main() {
+    int N; cin >> N;
+    vector<char> ch(N);
+    int out = 1;
+    for(int i = 1; i <= N; i++) {
+        ch[i-1] = '0' + out;
+        for(int j = 1; j < i; j++) {
+            if(i%j == 0 && ch[i-1] == ch[j-1]) {
+                ch[i-1] = '0' + ++out;
+            }
+        }
+        cout << ch[i-1] << " ";
+    }
+    cout << endl;
+    return 0;
+    
+}
