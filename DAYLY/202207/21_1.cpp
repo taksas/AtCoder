31e4915bc3f58c84bbcb3f57b835148ffc5fa525
taksas <takuminakayama4869@gmail.com>
2022-07-21 11:38:49 +0900

    2022/07/21_1

---
+++ DAYLY/202207/21_1.cpp
@@ -0,0 +1,92 @@
+// ARC078 - C
+// 21%
+// 1st
+// NOT_YET
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <tuple>
+#include <cassert>
+#include <bitset>
+
+
+// -----     cpp_snippets_v2     -----
+#define rep(i,n)                for (int i = 0; i < n; i++)
+#define repe(i,n)               for (int i = 0; i <= n; i++)
+#define rrep(i,n)               for (int i = n; i > 0; i--)
+#define rrepe(i,n)              for (int i = n; i >= 0; i--)
+#define rep1(i,n)               for (int i = 1; i < n; i++)
+#define rep1e(i,n)              for (int i = 1; i <= n; i++)
+#define rrep1(i,n)              for (int i = n; i > 1; i--)
+#define rrep1e(i,n)             for (int i = n; i >= 1; i--)
+
+#define vrep(X)                 for (auto&& x : X)
+
+#define vvii(X,n1,n2)           vector<vector<int>> X(n1,vector<int>(n2))
+#define vvllll(X,n1,n2)         vector<vector<long long int>> X(n1,vector<long long int>(n2))
+
+#define vpii(X)                 vector<pair<int, int>> X
+#define vpss(X)                 vector<pair<string, string>> X
+#define vpcc(X)                 vector<pair<char, char>> X
+#define vpis(X)                 vector<pair<int, string>> X
+#define vpic(X)                 vector<pair<int, char>> X
+#define vpsi(X)                 vector<pair<string, int>> X
+#define vpsc(X)                 vector<pair<string, char>> X
+#define vpci(X)                 vector<pair<char, int>> X
+#define vpcs(X)                 vector<pair<char, string>> X
+#define vint(X)                 vector<int> X
+#define vchar(X)                vector<string> X
+#define vstring(X)              vector<char> X
+
+#define bgen(X)                 begin(X), end(X)
+#define coutd(X)                cout << X << endl
+#define dcoutd(X)               if(DEBUGGING) cout << X << endl
+
+
+#define DEBUGGING 0
+
+using namespace std;
+
+int main() {
+    long long int N; cin >> N;
+    vector<int> A(N); vrep(A) cin >> x;
+    
+    sort(bgen(A));
+    
+    long long int point = 0;
+    long long int fir = A[0], las = 0;
+    for(int i = 1; i < N; i++) {
+        las += A[i];
+    }
+
+    long long int ret = abs(fir-las);
+    
+    if(N == 2) {
+        coutd(ret);
+        return 0;
+    }
+
+    while(1) {
+        if(point <= N-2) point++;
+        else break;
+        dcoutd(point);
+        fir += A[point];
+        las -= A[point];
+        
+        long long int temp = abs(fir-las);
+        ret = min(ret,temp);
+    }
+
+    coutd(ret);
+    return 0;
+    
+}
