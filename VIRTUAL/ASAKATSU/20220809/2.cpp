b8591d8ee1fb071cd0104f7fba5790adb08b6b82
tak_sas <takuminakayama4869@gmail.com>
2022-08-09 08:48:26 +0900

    2022/08/09_ASAKATSU

---
+++ VIRTUAL/ASAKATSU/20220809/2.cpp
@@ -0,0 +1,60 @@
+// ABC193 - C
+// ASAKATSU
+// rnd
+// NOT_YET
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <tuple>
+#include <cassert>
+#include <bitset>
+
+
+// -----     cpp_snippets_v6     -----
+#define rep(i,n)                for (int i = 0; i < n; i++)
+#define vrep(X)                 for (auto&& x : X)
+
+#define vvii(X,n1,n2)           vector<vector<int>> X(n1,vector<int>(n2))
+#define vpii(X)                 vector<pair<int, int>> X
+
+#define bgen(X)                 begin(X), end(X)
+#define coutd(X)                cout << X << endl
+#define chmax(X1, X2)           X1 = max(X1, X2)
+#define chmin(X1, X2)           X1 = min(X1, X2)
+
+#define dcoutd(X)               if(DEBUGGING) cout << X << endl
+#define ddefined(X)             if(DEBUGGING) { X }
+
+#define INF                     1e18
+#define ll                      long long int
+
+
+#define DEBUGGING 1
+
+using namespace std;
+
+
+int main() {
+    int N; cin >> N;
+    int ans = 0;
+
+    for(int i = 2; i <= N; i++) {
+        for(int j = 3; j <= N; j++) {
+            if(i%j == 0 && i != j) {
+                ans++;
+                break;
+            }
+        }
+    }
+    coutd(N-ans);
+
+}
