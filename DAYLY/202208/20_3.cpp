baddeff617d3e6a248436327d1d04b595e885174
tak_sas <takuminakayama4869@gmail.com>
2022-08-20 13:50:31 +0900

    2022/08/20_3

---
+++ DAYLY/202208/20_3.cpp
@@ -0,0 +1,64 @@
+// ABC003 - C
+// From_ASAKATSU
+// rnd
+// YAY!!_FAST
+
+
+// -----     cpp_snippets_v8.1.1     -----
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <tuple>
+#include <cassert>
+#include <bitset>
+#include <iomanip>
+
+
+#define rep(i,n)                for (int i = 0; i < n; i++)
+#define vrep(X)                 for (auto&& x : X)
+
+#define vvii(X,n1,n2)           vector<vector<int>> X(n1,vector<int>(n2))
+#define vpii(X)                 vector<pair<int, int>> X
+
+#define bgen(X)                 begin(X), end(X)
+#define coutd(X)                cout << X << endl
+#define coutds(N,X)             cout << fixed << setprecision(N) << X << endl
+#define chmax(X1, X2)           X1 = max(X1, X2)
+#define chmin(X1, X2)           X1 = min(X1, X2)
+#define nextP(X)                next_permutation(X.begin(), X.end())
+
+#define dcoutd(X)               if(DEBUGGING) cout << X << endl
+#define ddefined(Y)             if(DEBUGGING) ( Y )
+
+#define INF                     1e18
+#define ll                      long long int
+
+
+#define DEBUGGING 1
+
+using namespace std;
+
+int main() {
+    int N, K; cin >> N >> K;
+    vector<int> R(N); vrep(R) cin >> x;
+
+    sort(bgen(R));
+
+    double ans = 0;
+    int select = N-K;
+    rep(i,K) {
+        ans = (ans+R[select++]) / 2;
+    } 
+    
+    coutds(15,ans);
+    return 0;
+    
+}
