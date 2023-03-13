53cb328afd6e2380856ec53ee5b15d53a585fede
taksas <takuminakayama4869@gmail.com>
2023-03-14 07:24:01 +0900

    03/14

---
+++ DAYLY/202303/12_1.cpp
@@ -0,0 +1,76 @@
+// ABC293 - D
+// DIFF
+// REALTIME
+// COULDNOT
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <tuple>
+#include <cassert>
+#include <bitset>
+
+
+// -----     cpp_snippets_v2     -----
+#define rep(i,n)                for (int i = 0; i < n; i++)
+#define repe(i,n)               for (int i = 0; i <= n; i++)
+#define rrep(i,n)               for (int i = n; i > 0; i--)
+#define rrepe(i,n)              for (int i = n; i >= 0; i--)
+#define rep1(i,n)               for (int i = 1; i < n; i++)
+#define rep1e(i,n)              for (int i = 1; i <= n; i++)
+#define rrep1(i,n)              for (int i = n; i > 1; i--)
+#define rrep1e(i,n)             for (int i = n; i >= 1; i--)
+
+#define vrep(X)                 for (auto&& x : X)
+
+#define vvii(X,n1,n2)           vector<vector<int>> X(n1,vector<int>(n2))
+#define vvllll(X,n1,n2)         vector<vector<long long int>> X(n1,vector<long long int>(n2))
+
+#define vpii(X)                 vector<pair<int, int>> X
+#define vpss(X)                 vector<pair<string, string>> X
+#define vpcc(X)                 vector<pair<char, char>> X
+#define vpis(X)                 vector<pair<int, string>> X
+#define vpic(X)                 vector<pair<int, char>> X
+#define vpsi(X)                 vector<pair<string, int>> X
+#define vpsc(X)                 vector<pair<string, char>> X
+#define vpci(X)                 vector<pair<char, int>> X
+#define vpcs(X)                 vector<pair<char, string>> X
+#define vint(X)                 vector<int> X
+#define vchar(X)                vector<string> X
+#define vstring(X)              vector<char> X
+
+#define bgen(X)                 begin(X), end(X)
+#define coutd(X)                cout << X << endl
+#define dcoutd(X)               if(DEBUGGING) cout << X << endl
+
+
+#define DEBUGGING 1
+
+using namespace std;
+
+int main() {
+    int N, M; cin >> N >> M;
+    vector<vector<int>> graph(N,vector<int>(N));
+    for(int i = 1; i <= N; i++) for(int j = 1; j <= N; j++) {
+        graph[i][j] = 0;
+    }
+
+    for(int i = 0; i < M; i++) {
+        int A; cin >> A;
+        char temp; cin >> temp;
+        int C; cin >> C;
+        cin >> temp;
+        graph[A][C]
+    }
+    
+    return 0;
+    
+}
