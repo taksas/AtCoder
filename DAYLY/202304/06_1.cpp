fb24f8889bd4cdae7be0883a98a12a1758252636
taksas <takuminakayama4869@gmail.com>
2023-04-06 08:05:54 +0900

    04/06

---
+++ DAYLY/202304/06_1.cpp
@@ -0,0 +1,70 @@
+// ABL - C
+// 50%
+// 1st
+// YAY!!
+
+
+// -----     cpp_snippets_v8.5     -----
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <tuple>
+#include <cassert>
+#include <bitset>
+#include <iomanip>
+#include <sstream>
+
+
+
+#include <atcoder/all>
+
+
+
+#define rep(i,n)                for (int i = 0; i < n; i++)
+#define vrep(X)                 for (auto&& x : X)
+
+#define vvii(X,n1,n2)           vector<vector<int>> X(n1,vector<int>(n2))
+#define vvllll(X,n1,n2)         vector<vector<long long int>> X(n1,vector<long long int>(n2))
+
+#define vpii(X)                 vector<pair<int, int>> X
+#
+
+#define coutd(X)                cout << X << endl
+#define coutds(N,X)             cout << fixed << setprecision(N) << X << endl
+#define chmax(X1, X2)           X1 = max(X1, X2)
+#define chmin(X1, X2)           X1 = min(X1, X2)
+#define nextP(X)                next_permutation(X.begin(), X.end())
+
+#define INF                     8e18
+#define ll                      long long int
+
+
+using namespace std;
+using namespace atcoder;
+
+
+int main() {
+    int N, M; cin >> N >> M;
+    dsu uf(N);
+    
+    rep(i,M) {
+        int a, b; cin >> a >> b;
+        a--; b--;
+        uf.merge(a,b);
+    }
+    
+    cout << uf.groups().size()-1 << endl;
+    
+    return 0;
+    
+}
