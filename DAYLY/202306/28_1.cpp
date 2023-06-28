300fd32f9c9ca905fb600b87225c68d5e468bd49
taksas <takuminakayama4869@gmail.com>
2023-06-28 12:42:22 +0900

    28_1

---
+++ DAYLY/202306/28_1.cpp
@@ -0,0 +1,61 @@
+// ABC292 - C
+// DIRECT_RERUN
+// rnd
+// COPYED
+
+
+// -----     cpp_snippets_v8.10     -----
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <tuple>
+#include <cassert>
+#include <bitset>
+#include <iomanip>
+#include <sstream>
+
+#include <atcoder/all>
+
+#define MAXINT                  2147483647
+#define MININT                  -2147483648
+#define MAXLL                   9223372036854775807
+#define MINLL                   -9223372036854775808
+#define INFI                    (INT_MAX / 2)
+#define INFL                    (1LL << 60)
+#define ll                      long long int
+#define ull                     unsigned long long int
+
+#define rep(i,n)                for (int i = 0; i < n; i++)
+#define vrep(X)                 for (auto&& x : X)
+#define vvii(X,n1,n2)           vector<vector<int>> X(n1,vector<int>(n2))
+#define coutds(N,X)             cout << fixed << setprecision(N) << X << endl
+#define nextP(X)                next_permutation(X.begin(), X.end())
+
+using namespace std;
+using namespace atcoder;
+using mint = modint998244353;
+
+
+int main() {
+    int N; cin >> N;
+    int mp[200001] = {0};
+    ll ans = 0;
+    for(int i = 1; i < N; i++) for(int j = 1; j <= N/i; j++) mp[i*j]++;
+
+    for(int i = 1; i < N; i++) ans += mp[i] * mp[N-i];
+    
+    cout << ans << endl;
+    
+    return 0;
+    
+}
