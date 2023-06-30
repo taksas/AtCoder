8128f81d1a0123070b7caa18c9b3f008b07a9853
taksas <takuminakayama4869@gmail.com>
2023-06-30 12:55:22 +0900

    30_1

---
+++ DAYLY/202306/30_1.cpp
@@ -0,0 +1,69 @@
+// ABC255 - C
+// 50%
+// rnd
+// SHOULD?
+
+
+// -----     cpp_snippets_v8.11     -----
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <tuple>
+#include <cassert>
+#include <bitset>
+#include <iomanip>
+#include <sstream>
+
+#include <atcoder/all>
+
+#define MAXINT                  2147483647
+#define MININT                  -2147483648
+#define MAXLL                   9223372036854775807
+#define MINLL                   -9223372036854775808
+#define INFI                    (INT_MAX / 2)
+#define INFL                    (1LL << 60)
+#define ll                      long long int
+#define ull                     unsigned long long int
+
+#define rep(i,st,fn)            for (int i = st; i < fn; i++)
+#define vrep(X)                 for (auto&& x : X)
+#define vvii(X,n1,n2)           vector<vector<int>> X(n1,vector<int>(n2))
+#define coutds(N,X)             cout << fixed << setprecision(N) << X << endl
+#define nextP(X)                next_permutation(X.begin(), X.end())
+
+using namespace std;
+using namespace atcoder;
+using mint = modint998244353;
+
+
+int main() {
+    ll X, A, D, N; cin >> X >> A >> D >> N;
+    if(D<0) {
+        D = -D;
+        A -= D*(N-1);
+    }
+    
+    X -= A;
+
+    if(X > D*(N-1)) {
+        cout << X - D*(N-1) << endl;
+    } else if(X < 0) {
+        cout << -X << endl;
+    } else {
+        cout << (D != 0 ? min<ll>(X%D,D-(X%D)) : 0) << endl;
+    }
+    
+    
+    return 0;
+    
+}
