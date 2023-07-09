4047ec238e612b3474a68213ff6dfd8a30402571
taksas <takuminakayama4869@gmail.com>
2023-07-09 22:13:47 +0900

    20230709_ARC164

---
+++ A_Regular_C/20230709_ARC164/A.cpp
@@ -0,0 +1,67 @@
+// ARC164 - A
+// REALTIME
+// rnd
+// SHOULD?
+
+
+// -----     cpp_snippets_v8.12     -----
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <tuple>
+#include <cassert>
+#include <bitset>
+#include <iomanip>
+#include <sstream>
+
+#include <atcoder/all>
+
+#define MAXINT                  2147483647
+#define MININT                  -2147483648
+#define MAXLL                   9223372036854775807
+#define MINLL                   -9223372036854775808
+#define INFI                    (INT_MAX / 2)
+#define INFL                    (1LL << 60)
+
+#define rep(i,st,fn)            for (int i = st; i < fn; i++)
+#define rrep(i,st,fn)           for (int i = st; i >= fn; i--)
+#define vrep(X)                 for (auto&& x : X)
+#define vvii(X,n1,n2)           vector<vector<int>> X(n1,vector<int>(n2))
+
+using namespace std;
+using namespace atcoder;
+using mint0 = modint998244353;
+using mint1 = modint1000000007;
+
+
+int main() {
+    int T; cin >> T;
+    rep(io,0,T) {
+        long long int N, K; cin >> N >> K;
+        long long int Ntar = N-(N%3), target = 0;
+        unsigned long long int temp = 1;
+        while(temp < Ntar) {
+            temp *= 3;
+            target++;
+        }
+        cout << ((  target + N%3 <= K &&  )?"Yes":"No") << endl;
+        
+    }
+    
+    
+    
+    
+    
+    return 0;
+    
+}
